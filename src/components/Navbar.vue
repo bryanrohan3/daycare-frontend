61800369946d1b8b3d5542b47fedaf33890e5186
Bryan Rohan <bryan.rohan3@mail.dcu.ie>
2024-10-28 15:14:18 +0000

    repsonsive hamburger nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,21 @@
 <template>
-  <nav class="navbar fs-12 pl-30">
-    <ul class="mt-30">
-      <li class="mt-20" v-for="link in links" :key="link.name">
-        <router-link :to="link.path" active-class="active-link bold">
-          {{ link.label }}
-        </router-link>
-      </li>
-    </ul>
-  </nav>
+  <div>
+    <div class="hamburger" @click="toggleNavbar">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+
+    <nav :class="['navbar', { active: isNavbarActive }, 'fs-12', 'pl-30']">
+      <ul class="mt-30">
+        <li class="mt-20" v-for="link in links" :key="link.name">
+          <router-link :to="link.path" active-class="active-link bold">
+            {{ link.label }}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -17,6 +25,16 @@ export default {
     links: {
       type: Array,
       required: true,
+    },
+  },
+  data() {
+    return {
+      isNavbarActive: false,
+    };
+  },
+  methods: {
+    toggleNavbar() {
+      this.isNavbarActive = !this.isNavbarActive;
     },
   },
 };
--- src/views/SocialMediaFeedPage/SocialMediaFeedPage.vue
+++ src/views/SocialMediaFeedPage/SocialMediaFeedPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-30 align-center justify-center" @scroll.passive="handleScroll">
+  <div class="mt-20 align-center justify-center" @scroll.passive="handleScroll">
     <div v-if="posts.length > 0">
       <div class="fs-12 mb-30" v-for="post in posts" :key="post.id">
         <div class="flex-row-space">
